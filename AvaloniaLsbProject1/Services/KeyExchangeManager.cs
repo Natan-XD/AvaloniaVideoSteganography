753bac27020cf7dfc886eb91cec0cf8ebbb3ebb5
Matan Ofri <115949629+Natan-XD@users.noreply.github.com>
2025-05-13 11:21:43 +0300

    Update KeyExchangeManager.cs
    comments

--- AvaloniaLsbProject1/Services/KeyExchangeManager.cs
+++ AvaloniaLsbProject1/Services/KeyExchangeManager.cs
@@ -2,6 +2,7 @@
 using System.Numerics;
 using System.Security.Cryptography;
 using System.Globalization;
+//rewrite the comments
 namespace AvaloniaLsbProject1.Services
 {
     /// <summary>
@@ -11,7 +12,7 @@ namespace AvaloniaLsbProject1.Services
     public class KeyExchangeManager
     {
         // Diffie–Hellman parameters:
-        // A 256-bit prime (for demonstration purposes; in practice, choose a much larger safe prime)
+        // // 2048 -bit prime 
         private readonly BigInteger _prime;
         // A generator; here we use 2.
         private readonly BigInteger _generator;
@@ -50,6 +51,7 @@ namespace AvaloniaLsbProject1.Services
             DE2BCBF6 95581718 3995497C EA956AE5 15D22618 98FA0510
             15728E5A 8AACAA68 FFFFFFFF FFFFFFFF
                 */
+            //define this string
             _prime = BigInteger.Parse("FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AACAA68FFFFFFFFFFFFFFFF",
     NumberStyles.AllowHexSpecifier);
             _generator = 2;
